545fddde547ba2f3cfc1d5b41707667f69d9720c
James McCarthy <jamesmccarthyxci@gmail.com>
2019-12-21 18:27:46 -0500

    Constructor update

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -10,6 +10,17 @@ use Illuminate\Support\Facades\Storage;
 
 class ArtistsController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
